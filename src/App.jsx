b5b8cd321f2c4a960e612f7e842953afeb80288d
acf77 <antoniocarlos.j77@gmail.com>
2021-09-01 23:38:31 -0300

    ignite first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/global.scss";
+import "../src/components/RepositoryList";
+import { RepositoryList } from "../src/components/RepositoryList";
+
+export function App() {
+  return (
+    <>
+      <RepositoryList />
+      
+    </>
+  );
+}
